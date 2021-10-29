f741a4090381f43d471fe8515f362aa4f384bde4
Dehzxg <andre.santos610@outlook.com>
2021-10-29 15:49:09 -0300

    Adicionando CRUD

--- blogpessoal/src/main/java/br/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/br/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,5 +1,7 @@
 package br.com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import br.com.generation.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
-
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	// select * from tb_postagem where titulo like "%titulo%"
 }
